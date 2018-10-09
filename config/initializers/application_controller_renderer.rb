c1c2ba84915438086b5b86b583325c34fd589310
Sachiko8 <SachikoSuzuki815@gmail.com>
2018-10-09 06:49:12 +0000

    a

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'b2016aeba6276f7a71d369dc8f32b041243e539c0e11e17a9f47cf3909f5b56b2810bad6bcafbe2069823c1ba05915fbe8e73649589ecc48ebdf7600715df8b3'
